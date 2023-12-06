033241da4bd00a796db5b3f4a4a7cdd93da2adec
kanta1207 <kan0712livepool@gmail.com>
2023-12-06 15:01:46 -0800

    feat : implement basic auth feat

--- frontend/src/components/icons/Icons.tsx
+++ frontend/src/components/icons/Icons.tsx
@@ -1,9 +1,16 @@
-import { type LucideIcon, ShoppingCart, MenuSquare, User } from 'lucide-react';
+import {
+  type LucideIcon,
+  ShoppingCart,
+  MenuSquare,
+  User,
+  LogIn,
+} from 'lucide-react';
 
 export type Icon = LucideIcon;
 
 export const Icons = {
   shoppingCart: ShoppingCart,
   recipe: MenuSquare,
+  login: LogIn,
   user: User,
 };
