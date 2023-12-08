116dae0879824f87507f41a7f45af129ccd7dcdd
kanta1207 <kan0712livepool@gmail.com>
2023-12-08 15:27:08 -0800

    fix : api_key

--- frontend/src/api/route.ts
+++ frontend/src/api/route.ts
@@ -1,10 +1,16 @@
-export const apiKey = process.env.SPONACULAR_API_KEY;
+export const apiKey = process.env.SPOONACULAR_API_KEY;
 export const baseUrl = 'https://api.spoonacular.com/';
 
 const recipeBaseURL = baseUrl + 'recipes/';
+
+const jsonServerBaseURL = 'http://localhost:3005/';
+
+const fridge = 'fridge';
 
 const baseURLParams = `?apiKey=${apiKey}`;
 
 export const apiPath = {
   getRecipe: recipeBaseURL + 'complexSearch' + baseURLParams,
+  getItemsFromFridge: jsonServerBaseURL + fridge,
+  postItemToFridge: jsonServerBaseURL + fridge,
 };
