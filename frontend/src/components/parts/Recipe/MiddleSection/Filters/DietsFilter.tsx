c9d7aa86a4566e846132a97fe5f77d89cc44b9f9
kanta1207 <kan0712livepool@gmail.com>
2023-12-07 17:18:02 -0800

    Implement diets filter

--- frontend/src/components/parts/Recipe/MiddleSection/Filters/DietsFilter.tsx
+++ frontend/src/components/parts/Recipe/MiddleSection/Filters/DietsFilter.tsx
@@ -1,7 +1,24 @@
+'use client';
+
+import { usePathname, useSearchParams, useRouter } from 'next/navigation';
 import './filterStyling.css';
 import Image from 'next/image';
-const diets: string[] = ['Gluten', 'Vegan', 'Keto'];
+const diets: string[] = ['Gluten', 'Vegan', 'Keto'] as const;
+type Diet = (typeof diets)[number];
 export function DietsFilter() {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const handleSelectDiet = (diet: Diet) => {
+    const params = new URLSearchParams(searchParams);
+    if (diet) {
+      params.set('diets', diet);
+    } else {
+      params.delete('diets');
+    }
+    replace(`${pathname}?${params.toString()}`);
+  };
   return (
     <>
       <h1 className="intolerance-title mb-3">Diets</h1>
@@ -10,6 +27,7 @@ export function DietsFilter() {
           <div
             key={index}
             className="flex flex-col items-center hover:cursor-pointer"
+            onClick={() => handleSelectDiet(diet)}
           >
             <Image
               src={`/images/Filter/${diet}.png`}
