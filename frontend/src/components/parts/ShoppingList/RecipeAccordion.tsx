616804417f07f9d7ac13892a1558156971208c2f
Goktug YILDIZ <goktug.7tepe@gmail.com>
2023-12-12 18:41:11 -0800

    Issue/24 basic styling

--- frontend/src/components/parts/ShoppingList/RecipeAccordion.tsx
+++ frontend/src/components/parts/ShoppingList/RecipeAccordion.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import "../ShoppingList/RecipeAccordionStyling.css";
 import Image from "next/image";
 import {
   Accordion,
@@ -7,7 +8,6 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 import { Icons } from "@/components/icons/Icons";
-
 type Ingredient = {
   id: number;
   name: string;
@@ -70,13 +70,13 @@ const RecipeAccordion = ({
     <Accordion
       type="single"
       collapsible
-      className="w-full bg-input rounded-lg border p-4"
+      className="w-full bg-input rounded-lg border p-4 recipe-accordion"
       onClick={toggleOpen}
     >
       <AccordionItem value="item-1" className="border-none">
         <AccordionTrigger className={`${isOpen ? "border-b" : ""} text-muted`}>
           <div className="w-full flex justify-between items-center">
-            <p className="text-lg font-bold">{title}</p>
+            <p className="text-lg font-bold recipe-shadow">{title}</p>
             <div onClick={handleDelete}>
               <Icons.trash className="w-4 h-4 mr-4 text-black hover:stroke-primary" />
             </div>
@@ -84,13 +84,13 @@ const RecipeAccordion = ({
         </AccordionTrigger>
         <AccordionContent className="p-2">
           <div className="w-full flex justify-between gap-4">
-            <div className="w-2/3">
+            <div className="w-2/3 border-r pr-2 pt-2">
               <div
-                className="break-words"
+                className="break-words "
                 dangerouslySetInnerHTML={{ __html: summary }}
               />
               <div>
-                <h1 className="font-bold mt-4 text-primary text-xl">
+                <h1 className="font-bold mt-4 text-primary text-xl recipe-shadow">
                   Instructions
                 </h1>
                 {steps?.map((step, index) => (
@@ -104,8 +104,8 @@ const RecipeAccordion = ({
               </div>
             </div>
 
-            <div className="w-1/3 flex flex-col border-l pl-3">
-              <div className="h-32 border-b pb-3 mb-4">
+            <div className="w-1/3 flex flex-col pl-3">
+              <div className="h-32 pb-3 mb-4 pt-2">
                 <Image
                   src={imageUrl || ""}
                   width={200}
@@ -116,22 +116,34 @@ const RecipeAccordion = ({
                 />
               </div>
               <div>
-                <p className="text-sm font-semibold mb-2">Diets:</p>
-                <ul className="list-disc list-inside">{renderList(diets)}</ul>
-                <p className="text-sm font-semibold mt-4 mb-2">Intolerances:</p>
-                <ul className="list-disc list-inside">
-                  {intolerance && renderList(intolerance)}
-                </ul>
-                <p className="text-sm font-semibold mb-2">Ingredients</p>
-                {uniqueIngredients.length > 0 ? (
-                  <ul className="list-disc list-inside">
-                    {uniqueIngredients?.map((ingredient, i) => (
-                      <li key={i}>{ingredient.name}</li>
-                    ))}
+                <div className="mb-4">
+                  <p className="text-sm font-semibold mb-1 underline underline-offset-2">
+                    Diets:
+                  </p>
+                  <ul className="list-inside">{renderList(diets)}</ul>
+                </div>
+                <div className="mb-4  ">
+                  <p className="text-sm font-semibold mt-4 mb-1 underline underline-offset-2">
+                    Intolerances:
+                  </p>
+                  <ul className="list-inside">
+                    {intolerance && renderList(intolerance)}
                   </ul>
-                ) : (
-                  <p>No ingredients listed for this step.</p>
-                )}
+                </div>
+                <div className="mb-2">
+                  <p className="text-sm font-semibold mb-1 underline underline-offset-4">
+                    Ingredients:
+                  </p>
+                  {uniqueIngredients.length > 0 ? (
+                    <ul className="list-disc list-inside">
+                      {uniqueIngredients?.map((ingredient, i) => (
+                        <li key={i}>{ingredient.name}</li>
+                      ))}
+                    </ul>
+                  ) : (
+                    <p>No ingredients found</p>
+                  )}
+                </div>
               </div>
             </div>
           </div>
